22ba75ab50841018a659857e88e3679a742d6204
dunnoconnor <moconnor8@gmail.com>
2021-12-02 16:56:48 -0500

    bulkCreate

--- index.test.js
+++ index.test.js
@@ -1,21 +1,43 @@
 const {sequelize} = require('./db')
 const {Musician} = require('./index')
+
+//test musician database CRUD
 describe('Muscian Database', () => {
+    //force sync database
     beforeAll(async() => {
         await sequelize.sync({force:true})
     })
 
-    test('can create a musician', async() => {
-        await Musician.create({name: 'Prince', instrument: 'guitar'})
+    //create instances of Musician Model for testing
+    const arrayOfMusicians =[
+        {name: 'Prince', instrument: 'guitar'},
+        {name: 'Kasem', instrument: 'dhol'}
+    ]
+    
+    test('musicians have name', async() => {
+        await Musician.bulkCreate(arrayOfMusicians)
+        // await Musician.create({name: 'Prince', instrument: 'guitar'})
         // const musicians = await Musician.findAll();
-        // console.log(musicians)
+        //read test instance from db
         const testMusician = await Musician.findOne({
             where: {
               name: 'Prince'
             }
           });
         expect(testMusician.name).toBe('Prince')
-        expect(testMusician.instrument).toBe('guitar')
-    
     })
+
+    test('musicians have an instrument', async() => {
+        await Musician.bulkCreate(arrayOfMusicians)
+        // await Musician.create({name: 'Prince', instrument: 'guitar'})
+        // const musicians = await Musician.findAll();
+        //read test instance from db
+        const testMusician = await Musician.findOne({
+            where: {
+              name: 'Prince'
+            }
+          });
+        expect(testMusician.instrument).toBe('guitar')
+    })
+
 })
